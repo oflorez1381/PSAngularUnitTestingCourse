9c845a124653645ed393731a76d4b58b24af6079
oflorez1381 <oscarflorez1381@gmail.com>
2019-08-27 21:32:54 -0400

    4.4 Using NO_ERRORS_SCHEMA to hide html errors

---
+++ src/app/hero/hero.component.shallow.spec.ts
@@ -0,0 +1,21 @@
+import {ComponentFixture, TestBed} from '@angular/core/testing';
+import { HeroComponent } from './hero.component';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+
+describe('Hero Component (shallow tests)', () => {
+  let fixture: ComponentFixture<HeroComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [HeroComponent],
+      schemas: [NO_ERRORS_SCHEMA]
+    });
+    fixture = TestBed.createComponent(HeroComponent);
+  });
+
+  it('should have the correct hero', () => {
+    fixture.componentInstance.hero = { id: 1, name: 'SuperDude', strength: 3};
+
+    expect(fixture.componentInstance.hero.name).toEqual('SuperDude');
+  });
+});
