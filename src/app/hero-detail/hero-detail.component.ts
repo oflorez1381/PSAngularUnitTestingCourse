3ef0bb37f2a8595e691bc0906584e6190a73f9ca
oflorez1381 <oscarflorez1381@gmail.com>
2019-09-04 20:33:39 -0400

    6.7  Testing with ActivatedRoute

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Input } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
 
+// tslint:disable-next-line:import-spacing
 import { Hero }         from '../hero';
 import { HeroService }  from '../hero.service';
 
